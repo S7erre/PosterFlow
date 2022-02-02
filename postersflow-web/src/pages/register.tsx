c82fd3c78cca4c0f01c1baad02ca7f5819baf674
Stefan <Stefan.Markovic950@gmail.com>
2022-02-02 23:05:51 +0100

    nextjs chakra-ui

---
+++ postersflow-web/src/pages/register.tsx
@@ -0,0 +1,39 @@
+import React from 'react';
+import { Formik, Form } from "formik";
+import { FormControl, FormLabel, Input, FormErrorMessage } from '@chakra-ui/react';
+import { Wrapper } from '../components/Wrapper';
+import { InputField } from '../components/InputField';
+
+interface registerProps {}
+
+export const Register: React.FC<registerProps> = ({}) => {
+    return (
+        <Wrapper variant="small">
+            <Formik 
+                initialValues={{ username: "", password: "" }}
+                onSubmit={(values) => {
+                    console.log(values);
+                }}
+            
+            >
+                {({ values, handleChange }) => (
+                    <Form>
+                        <InputField
+                            name="username"
+                            placeholder="username"
+                            label="Username"
+                        />
+                        <InputField
+                            name="password" 
+                            placeholder="password"
+                            label="password"
+                            type="password"
+                        />
+                    </Form>
+                )}
+            </Formik>
+        </Wrapper>
+    );
+};
+
+export default Register;
