64278d3087017a3f232a6d6bc0af30129bdd0e02
Stefan <Stefan.Markovic950@gmail.com>
2022-02-04 23:45:04 +0100

    fix

--- src/index.ts
+++ src/index.ts
@@ -26,7 +26,7 @@ const main = async () => {
     app.set('trust proxy', 1);
     app.use(
         cors({
-            origin: "http://localhost:3000",
+            origin: "http://localhost:4000",
             credentials: true,
         })
     );
